495d6b49bf37978863f17b7ae7b6f55338ee765f
Ragaey <mragaey@arkdev.net>
2018-05-30 16:17:49 +0200

    updating file structure

--- _site/src/js/require-label.js
+++ _site/src/js/require-label.js
@@ -1,30 +1,9 @@
 /**
  * Required labels js
  */
-/* global $ */
 try {
-  window.requiredLabels = function (form) {
-    $(form).find('input,select,textarea').each(function () {
-      if ($(this).prop('required')) {
-        $(this).siblings('label').addClass('required-label')
-        /*
-         This code will check if the input is required but have no label
-         then will create a label and add it to the input
-         */
-        if (($(this).siblings('label').length <= 0)) {
-          console.log('label')
-          $(this).parent().prepend('<label>' + $(this).attr('placeholder') + '</label>')
-          $(this).siblings('label').addClass('required-label')
-        }
-        /*
-         This is if Input is wrapped in a label
-         */
-        if (($(this).parent('label').length > 0)) {
-          $(this).siblings('label').remove()
-        }
-      }
-    })
-  }
+  window.$ = window.jQuery = require('jquery')
+  require('required-labels')
 } catch (e) {
   console.log('error' + e.message)
 }
