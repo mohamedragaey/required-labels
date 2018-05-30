e127a5fe389d36d34c789478461e0cbb6800553e
Mohamed <mohamedragaey33@gmail.com>
2018-05-30 22:31:55 +0200

    Adding RTL-LTR support

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,6 @@
 const webpack = require('webpack')
 const merge = require('webpack-merge')
 const common = require('./webpack.common.js')
-const WebpackRTLPlugin = require('webpack-rtl-plugin')
 module.exports = merge(common, {
   devtool: 'inline-source-map',
   devServer: {
@@ -24,11 +23,6 @@ module.exports = merge(common, {
     port: 3000
   },
   plugins: [
-    new WebpackRTLPlugin({
-      filename: 'css/app-rtl.css',
-      diffOnly: false,
-      minify: false
-    }),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('development')
     })
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,6 @@
 const webpack = require('webpack')
 const merge = require('webpack-merge')
 const common = require('./webpack.common.js')
-const WebpackRTLPlugin = require('webpack-rtl-plugin')
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
 module.exports = merge(common, {
@@ -9,11 +8,6 @@ module.exports = merge(common, {
     new UglifyJsPlugin({
       cache: true,
       parallel: true
-    }),
-    new WebpackRTLPlugin({
-      filename: 'css/app-rtl.css',
-      diffOnly: false,
-      minify: true
     }),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production')
